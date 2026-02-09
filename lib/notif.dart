9912af440e51b05757a1bd3c23010726aa8f368b
Ariyer Nawer <purnota@Ariyers-MacBook-Air.local>
2026-02-09 23:19:45 +0600

    message

---
+++ lib/notif.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class notif extends StatefulWidget {
+  const notif({super.key});
+
+  @override
+  State<notif> createState() => _notifState();
+}
+
+class _notifState extends State<notif> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+
+      ),
+    );
+  }
+}
